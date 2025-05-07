0066eb74957a033a966d6e8e18e35f1d2ef26b81
Hugo Baizan <hbaizan@gmail.com>
2025-05-07 20:07:16 -0300

    cors error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [{
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+        { key: "Access-Control-Allow-Headers", value: "X-Requested-With, X-CSRF-Token, Authorization" },
+        { key: "Access-Control-Allow-Credentials", value: "true" }
+      ]
+    }]
+  }
 };
 
 export default nextConfig;
