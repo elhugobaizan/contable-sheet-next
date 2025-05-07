970313570a192c368cc83e80ef7cf3523558bfa6
Hugo Baizan <hbaizan@gmail.com>
2025-05-07 11:48:53 -0300

    first version

---
+++ app/bancos/[id]/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest as Req, NextResponse as Res } from "next/server";
+
+export async function GET(req: Req, { params }: { params: Promise<{ id: string }> }) {
+    console.log("Leer banco");
+    try {  
+        const id = (await params).id;
+        const result = {obj: `await prisma.banco.findMany({where: { id: ${id} }})`};
+        return Res.json(result);
+    } catch (err) {
+        return Res.json({message: err});
+    }
+}
+
+export async function UPDATE(req: Req, { params }: { params: Promise<{ id: string }> }) {
+  console.log("Actualizar banco");  
+  try {
+      const id = (await params).id;
+      const body = await req.json();
+      const { name, capital, period, tna, logo, duedate } = body;
+      const result = {obj: `await prisma.banco.update({ where: { id: ${id} }, data: { name, capital, period, tna, logo, duedate } })`};
+      return Res.json(result);
+  } catch (err) {
+      return Res.json({message: err});
+  }    
+}
+
+export async function DELETE(req: Req, { params }: { params: Promise<{ id: string }> }) {
+  console.log("borrar banco");  
+  try {
+      const id = (await params).id;
+      const result = {obj: `await prisma.banco.delete({
+          where: {
+              id: id
+          }
+      })`};
+      return Res.json(result);
+  } catch (err) {
+      return Res.json({message: err});
+  }
+}
---
+++ app/bancos/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest as Req, NextResponse as Res } from "next/server";
+
+//List bancos
+export async function GET() {
+    console.log("listar bancos");
+    try {  
+        const result = { obj: 'await prisma.banco.findMany()' };
+        console.log("find many");
+        return Res.json(result);
+    } catch (err) {
+        console.log("hay error", err);
+        return Res.json(err);
+    }
+}
+
+//Create banco
+export async function POST(req: Req) {
+    console.log("crear nuevo banco");
+    try {
+        const body = await req.json();
+        const { name, capital, period, tna, logo, duedate } = body;
+        const result = {
+          name,
+          capital,
+          period,
+          tna,
+          logo,
+          duedate
+        };
+        return Res.json(result);
+    } catch (err) {
+        return Res.json({message: err});
+    }
+}
+
