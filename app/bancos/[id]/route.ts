e01f2e7b562450e415061291f2be59617c9368f0
Hugo Baizan <hbaizan@gmail.com>
2025-05-08 19:01:05 -0300

    change field names

--- app/bancos/[id]/route.ts
+++ app/bancos/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest as Req, NextResponse as Res } from "next/server";
-import { prisma } from '@/db'
+import { prisma } from '@/db';
 
 export async function GET(req: Req, { params }: { params: Promise<{ id: string }> }) {
     console.log("Leer banco");
@@ -17,8 +17,13 @@ export async function PUT(req: Req, { params }: { params: Promise<{ id: string }
     try {
         const id = (await params).id;
         const body = await req.json();
-        const { name, capital, period, tna, logo, duedate } = body;
-        const result = { obj: `await prisma.banco.update({ where: { id: ${id} }, data: { name, capital, period, tna, logo, duedate } })` };
+        const { nombre, capital, periodo, tna, logo, vencimiento } = body;
+        console.log("body", body);
+
+        const result = await prisma.banco.update({
+            where: { id: +id },
+            data: { name: nombre, capital, period: periodo, tna, logo, duedate: vencimiento }
+        });
         return Res.json(result);
     } catch (err) {
         return Res.json({ message: err });
@@ -29,12 +34,11 @@ export async function DELETE(req: Req, { params }: { params: Promise<{ id: strin
     console.log("borrar banco");
     try {
         const id = (await params).id;
-        const result = {
-            obj: `await prisma.banco.delete({
-          where: {
-              id: id
-          }
-      })`};
+        const result = await prisma.banco.delete({
+            where: {
+                id: +id
+            }
+        });
         return Res.json(result);
     } catch (err) {
         return Res.json({ message: err });
