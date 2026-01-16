3432b5d0ebdc34b82a73fb2fa876fbfd93d3e39a
Hugo Baizan <hbaizan@aconcaguasf.com>
2026-01-16 14:52:08 -0300

    Arreglado el endpoint de pago

--- app/fijos/[id]/pay/route.ts
+++ app/fijos/[id]/pay/route.ts
@@ -14,7 +14,7 @@ export async function PUT(req: Req, { params }: { params: Promise<{ id: string }
 
         const updateData: any = {
             Deuda: 0,
-            Vencimiento: new Date("1970-01-01T03:00:00.000+00:0"),
+            Vencimiento: new Date("1970-01-01T00:00:00.000+00:00"),
         };
         
 
