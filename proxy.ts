cb4cb9e7717bb8711026d6971aed05fee2c5aa01
Hugo Baizan <hbaizan@aconcaguasf.com>
2026-02-23 15:43:56 -0300

    Agregada la nueva URL al whitelist de CORS

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const allowedOrigins = new Set(['http://localhost:8081', 'https://hb-sheet-contable.vercel.app', 'http://localhost:8082', 'http://localhost:8083'])
+const allowedOrigins = new Set([
+  'http://localhost:8081', 
+  'http://localhost:8082', 
+  'http://localhost:8083', 
+  'https://hb-sheet-contable.vercel.app',
+  'https://hbcompass.vercel.app'])
 
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
