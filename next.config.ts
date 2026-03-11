e6d6f2d5233edbc8dc4277b6277943e427ae8f69
Hugo Baizan <elhugobaizan@gmail.com>
2026-03-10 21:15:30 -0300

    a ver esto

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [{ source: "/api/:path*", destination: "/:path*" }];
+    return [{
+      source: "/api/:path*", destination: "/:path*", headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+        { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+      ]
+    }];
   },
 };
 
