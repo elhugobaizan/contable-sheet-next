1a31ef73e0da49665f09d02366d6fea3360b3d15
Hugo Baizan <elhugobaizan@gmail.com>
2026-03-10 21:20:04 -0300

    volver a next 16 a ver si lo soluciona

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [{
-      source: "/api/:path*", destination: "/:path*", headers: [
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-        { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-      ]
-    }];
+    return [{ source: "/api/:path*", destination: "/:path*" }];
   },
 };
 
